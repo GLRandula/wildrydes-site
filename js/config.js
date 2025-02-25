d274c17684563f3572cc6aa1e25450cd907518e0
Lakith Randula <110283259+GLRandula@users.noreply.github.com>
2025-02-25 08:02:20 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1_ZXCayT8Q0', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'p880fqj1dv751o4c2ei2n2ufs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
